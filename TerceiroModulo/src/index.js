1a0bedd6d3aaf43f51830388f98369eab4ba613a
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-04-22 21:13:04 -0300

    Primeira versão metodo fibonacci - JavaScript

--- TerceiroModulo/src/index.js
+++ TerceiroModulo/src/index.js
@@ -1,10 +1,24 @@
 'use strict'
 
-const fibonacci = () => null
+const fibonacci = () => {
+    let listFibonacci = [0,1]
+    let i = 0
+    while(listFibonacci[i] <= 350){
+        listFibonacci.push(listFibonacci[i] + listFibonacci[i + 1])
+        console.log(listFibonacci[i])
+        i++
+    }
+}
 
-const isFibonnaci = (num) => null
+const isFibonnaci = (num) => {
+    
+}
 
 module.exports = {
     fibonacci,
     isFibonnaci
 }
+
+
+fibonacci()
+
