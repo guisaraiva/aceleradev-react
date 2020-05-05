f9c903fef4d5210306124cd5b6014950c032af29
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-05-04 21:45:26 -0300

    Novas alterações

--- QuartoModulo/src/index.js
+++ QuartoModulo/src/index.js
@@ -1,7 +1,28 @@
+const { products } = require("../src/data/products");
+
+
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
-function getShoppingCart(ids, productsList) {
-	return {};
+function getShoppingCart(ids,productsList) {
+	
+		const result =  productsList.filter(produto => produto.id == ids)
+	
+	
+
+	return (
+	 //productsList.filter(produto => produto.id === id).filter(produto => produto.name)
+	
+
+result
+	 //console.log(result.name)
+	 
+	)
+	;
 }
 
-module.exports = { getShoppingCart };
+/*module.exports = { getShoppingCart };*/
+
+/*console.log(products.filter(produto => produto.name === "PINK PANTHER™ T-SHIRT"))*/
+
+console.log(getShoppingCart(110, products))
+
