d5eb401f66dc5288d323b9082ed0ba18d9c2cf7a
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-05-24 20:36:44 -0300

    Import do Dayjs para formatação da data.

--- SextoModulo/src/components/Contact.jsx
+++ SextoModulo/src/components/Contact.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import dayjs from 'dayjs';
 export default class Contact extends React.Component {
-  dateFormatter = date => {
-    const dateParser = new Intl.DateTimeFormat('pt-br');
-    return dateParser.format(new Date(date));
-  }
-  
+
   render() {
     const data = this.props.data; //Carrega os dados do arquivo JSON através do props.
+    const { admissionDate } = this.props.data
+    const date = !admissionDate ? "" : dayjs(admissionDate).format('DD/MM/YYYY');
     return (
       <article data-testid ="contact" className="contact">
         <img className="contact__avatar" src={data.avatar} alt={data.name}/>
         <span className="contact__data" data-testid="contact-name">{data.name}</span>
         <span className="contact__data" data-testid="contact-phone">{data.phone}</span>
         <span className="contact__data" data-testid="contact-country">{data.country}</span>
-        <span className="contact__data" data-testid="contact-date">{this.dateFormatter(data.admissionDate)}</span>
+        <span className="contact__data" data-testid="contact-date">{date}</span>
         <span className="contact__data" data-testid="contact-company">{data.company}</span>
         <span className="contact__data" data-testid="contact-department">{data.department}</span>
       </article>
