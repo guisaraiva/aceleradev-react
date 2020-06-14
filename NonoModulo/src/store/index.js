47747f5783b177e986e0ae6061cba53445ee1d42
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-06-13 21:13:03 -0300

    Correção de arquivo index.js

--- NonoModulo/src/store/index.js
+++ NonoModulo/src/store/index.js
@@ -3,10 +3,6 @@ import { createStore } from 'redux';
 import persistReducers from './persistisReducers';
 import rootReducer from './modules/rootReducer';
 
-const enhancer =
-  process.env.NODE_ENV === 'development'
-    ? console.tron.createEnhancer()
-    : null;
 const store = createStore(persistReducers(rootReducer),
 );
 const persistor = persistStore(store);
